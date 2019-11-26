e67ed3811e6adf701be7387dd2565bdeef861d10
lzkmylz <lzkmylz@163.com>
2019-11-26 20:50:35 +0800

    finish adf test need find stock that can pass adf test

--- Statistics/similarity_compute.py
+++ Statistics/similarity_compute.py
@@ -1,15 +1,58 @@
 import json
+import os
+import tushare as ts
+import numpy as np
+from statsmodels.tsa.stattools import adfuller
+import statsmodels.api as sm
 
+token = os.environ['TUSHARE_TOKEN']
+pro = ts.pro_api(token)
 
 def similarity_compute(event, context):
-    body = {
-        "message": "Go Serverless v1.0! Your function executed successfully!",
-        "input": event
-    }
+    request_data = json.loads(event['body'])
+    stock1 = request_data['stock1']
+    stock2 = request_data['stock2']
+    start_date = request_data['startDate']
+    end_date = request_data['endDate']
+
+    adf_flag = False
+
+    stock1_data = pro.daily(ts_code=stock1, start_date=start_date, end_date=end_date)
+    stock2_data = pro.daily(ts_code=stock2, start_date=start_date, end_date=end_date)
+
+    stock1_prices = np.array(stock1_data['close'])
+    stock2_prices = np.array(stock2_data['close'])
+
+    stock1_adf = adfuller(stock1_prices)
+    stock2_adf = adfuller(stock2_prices)
+
+    if stock1_adf[0] > stock1_adf[4]['5%']:
+        adf_flag = True
+    if stock2_adf[0] > stock2_adf[4]['5%']:
+        adf_flag = True
+
+    if adf_flag:
+        response = {
+            "statusCode": 200,
+            "body": json.dumps({
+                "score": 0
+            })
+        }
+        return response
+
+    model = sm.OLS(stock1_prices, stock2_prices)
+    results = model.fit()
+    params = results.params
+    alpha = params[0]
+    beta = params[1]
+    sigma = np.sum(stock1_prices - (stock2_prices * alpha + beta))
+    score = (1 - sigma / np.mean(stock2_prices)) * 100
 
     response = {
         "statusCode": 200,
-        "body": json.dumps(body)
+        "body": json.dumps({
+            "score": score
+        })
     }
 
     return response
