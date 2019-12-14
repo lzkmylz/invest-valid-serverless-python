92757c9274493bf8711f5deec41ff8259b8040fc
lzkmylz <lzkmylz@163.com>
2019-12-14 11:29:55 +0800

    start to adjust model

--- dl_gru_ah/train_model.py
+++ dl_gru_ah/train_model.py
@@ -1,17 +1,25 @@
+from __future__ import absolute_import
 import tushare as ts
 import pandas as pd
+import tensorflow as tf
+import numpy as np
 from datetime import date, datetime, timedelta
 import os
 
 token = os.environ['TUSHARE_TOKEN']
 pro = ts.pro_api(token)
-
+tf.enable_eager_execution()
 
 stock_A = "601939.SH"
 stock_H = "00939.HK"
 backword_length = 5
+forword_length = 1
+BATCH_SIZE = 16
+BUFFER_SIZE = 10000
+EPOCHS = 50
+EVALUATION_INTERVAL = 200
 end_date = date.today().strftime("%Y%m%d")
-start_date = datetime.now() - timedelta(days=200)
+start_date = datetime.now() - timedelta(days=365)
 start_date = start_date.strftime("%Y%m%d")
 
 stock_data_A = pro.daily(ts_code=stock_A, start_date=start_date, end_date=end_date)
@@ -44,7 +52,7 @@ pct_chg_std = stock_data_A_frame['pct_chg'][:TRAIN_SPLIT].std()
 amount_std = stock_data_A_frame['amount'][:TRAIN_SPLIT].std()
 
 for index, row in stock_data_A_frame.iterrows():
-    if index < (len(stock_data_A_frame) - backword_length):
+    if index < (len(stock_data_A_frame) - backword_length - forword_length):
         single_data = []
         for i in range(backword_length):
             data_row = stock_data_A_frame.iloc[index + i]
@@ -60,12 +68,45 @@ for index, row in stock_data_A_frame.iterrows():
             ]
             single_data.append(single_row_data)
         data.append(single_data)
-        predict_row = stock_data_A_frame.iloc[index + backword_length]
-        label.append(predict_row['change'])
+
+        single_predict = []
+        for j in range(backword_length, backword_length + forword_length):
+            predict_row = stock_data_A_frame.iloc[index + j]
+            if predict_row['change'] > 0:
+                single_predict.append(1)
+            else:
+                single_predict.append(0)
+        label.append(single_predict)
 
 train_x = data[:TRAIN_SPLIT]
 train_y = label[:TRAIN_SPLIT]
 test_x = data[TRAIN_SPLIT:]
 test_y = label[TRAIN_SPLIT:]
+
+train_data_single = tf.data.Dataset.from_tensor_slices((train_x, train_y))
+train_data_single = train_data_single.cache().shuffle(BUFFER_SIZE).batch(1).repeat()
+
+val_data_single = tf.data.Dataset.from_tensor_slices((test_x, test_y))
+val_data_single = val_data_single.batch(BATCH_SIZE).repeat()
+
+# define model
+model = tf.keras.Sequential()
+model.add(tf.keras.layers.LSTM(64, input_shape=(backword_length, 8), return_sequences=True))
+model.add(tf.keras.layers.Dropout(0.2))
+model.add(tf.keras.layers.LSTM(128, return_sequences=False))
+model.add(tf.keras.layers.Dropout(0.2))
+model.add(tf.keras.layers.Dense(forword_length, activation="sigmoid"))
+model.compile(optimizer=tf.keras.optimizers.RMSprop(), loss='mae', metrics=['accuracy'])
+
+for x, y in val_data_single.take(1):
+    x = np.array(x)
+    y = np.array(y)
+    print(x.shape)
+    print(y.shape)
+
+model_history = model.fit(train_data_single, epochs=EPOCHS,
+                          steps_per_epoch=EVALUATION_INTERVAL,
+                          validation_data=val_data_single,
+                          validation_steps=50)
 
 
